f33951380950ae49b9b173801fef026bbc1b27ce
Rajiv Kumar <rajivk@gentrack.com>
2020-08-06 13:22:39 +0100

    work on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,65 +1,40 @@
 import React from "react";
 import styled from "styled-components";
-import { navLinks, colors } from '@config';
-import { mixins } from '@styles';
+import { navLinks, colors } from '../config';
 
-const Nav = styled.div`
-  background-color: #0a192f;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.9975);
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 10;
-  outline: 0;
+const StyledContainer = styled.div`
+  background-color: #020c1b;
+  display: flex;
+  border: 1px solid #1C6EA4;
+  justify-content: space-around;
 `
-
-const Sidebar = styled.aside`
-  ${mixins.flexCenter};
-  flex-direction: column;
-  background-color: ${colors.lightNavy};
-  padding: 50px;
-  width: 50vw;
-  height: 100%;
-  position: relative;
-  right: 0;
-  margin-left: auto;
-`
-
-const NavLinks = styled.nav`
-  ${mixins.flexBetween};
-  color: #0a192f;
-  flex-direction: column;
-  width: 100%;
-  text-align: center;
-`
-
 const NavList = styled.ol`
-  padding: 0;
-  margin: 0;
-  list-style: none;
-  width: 100%;
-`
+  display: flex;
+  border: 1px solid #1C6EA4;
+  margin: 25px 25px 25px 25px;
 
+`
 const NavListItem = styled.li`
   color: #64ffda;
-  margin: 0 auto 20px;
-  position: relative;
+  display: flex;
+  border: 1px solid #1C6EA4;
+  margin: 10px 10px 10px 10px
 `
+const MenuLink = styled.a``
 
-const MenuLink = styled.a`
-  ${mixins.link};
-  padding: 3px 20px 20px;
-  width: 100%;
+const StyledName = styled.h3`
+  color: #64ffda;
+  display: flex;
+  border: 1px solid #1C6EA4;
+  margin: 10px 10px 10px 10px
 `
 
 function Header() {
   return (
-    <Nav>
-      <Sidebar>
-        <NavLinks>
+    <StyledContainer>
+      <StyledName>
+        Rajiv Kumar
+      </StyledName>
           <NavList>
             {navLinks.map(({ url, name }) => (
               <NavListItem>
@@ -67,9 +42,7 @@ function Header() {
               </NavListItem>
             ))}
           </NavList>
-        </NavLinks>
-      </Sidebar>
-    </Nav>
+    </StyledContainer>
   )
 }
 
