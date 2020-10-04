bdc3d2b18c91d82abb763d835165359c6f5abc91
Rajiv Kumar <rajivk@gentrack.com>
2020-10-04 15:27:58 +0100

    added resume  link

--- src/styles/Section.js
+++ src/styles/Section.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Section = styled.section`
-  margin: 0 auto;
-  padding: 0px 0px 150px 0px;
+  margin: auto;
+  padding: 100px 0px 150px 0px;
   max-width: 1000px;
 `;
 
