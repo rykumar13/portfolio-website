6f48bba0c52a3d2a33b2d42d096c9a6cdc5ace9d
Rajiv Kumar <rajivk@gentrack.com>
2020-10-24 01:09:27 +0100

    added home link to logo

--- src/components/header.js
+++ src/components/header.js
@@ -3,21 +3,21 @@ import { IconLogo } from "@components/icons"
 import styled from "styled-components"
 import { navLinks } from "@config"
 import { theme, mixins } from "@styles"
-import { Link } from "react-scroll"
+import { Link } from "gatsby"
 import Headroom from "react-headroom"
 const { colors, fontSizes, fonts } = theme
 
 const StyledContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   top: 0;
   background-color: ${colors.white};
   height: 80px;
-  @media(max-width: 63em) {
+  @media (max-width: 63em) {
     padding: 0 40px;
   }
-  @media(max-width: 48em) {
+  @media (max-width: 48em) {
     padding: 0 25px;
   }
 `
@@ -40,12 +40,12 @@ const MenuLink = styled.a`
 `
 
 const StyledResumeButton = styled.a`
-display: flex;
-align-items: center;
-${mixins.smallButton};
+  display: flex;
+  align-items: center;
+  ${mixins.smallButton};
   font-size: ${fontSizes.smish};
   margin: 40px 50px 50px 50px;
-  @media(max-width: 48em) {
+  @media (max-width: 48em) {
     margin: 40px -10px 50px 50px;
   }
 `
@@ -68,46 +68,52 @@ const StyledLogo = styled.div`
       user-select: none;
     }
   }
-  @media(max-width: 48em) {
+  @media (max-width: 48em) {
     margin: 45px 50px 50px 50px;
-    margin-left: -15px
+    margin-left: -15px;
   }
 `
+
 const StyledLink = styled.div`
   display: flex;
   align-items: center;
-  @media(max-width: 48em) {
+  @media (max-width: 48em) {
     display: none;
   }
-`;
+`
+// https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
+if (typeof window !== "undefined") {
+  // eslint-disable-next-line global-require
+  require("smooth-scroll")('a[href*="#"]')
+}
 
 const Header = () => {
   return (
     <Headroom>
-      <StyledContainer>
+      <StyledContainer id="home">
         <StyledLogo tabindex="-1">
-          <a href="/" aria-label="home">
+          <Link to="/#home" aria-label="home">
             <IconLogo />
-          </a>
+          </Link>
         </StyledLogo>
         <StyledLink>
-        <NavList>
-          {navLinks.map(({ url, name }) => (
-            <NavListItem>
-              <MenuLink>
-                <Link
-                  activeClass="active"
-                  to={url}
-                  spy={true}
-                  smooth={true}
-                  duration={700}
-                >
-                  {name}
-                </Link>
-              </MenuLink>
-            </NavListItem>
-          ))}
-        </NavList>
+          <NavList>
+            {navLinks.map(({ url, name }) => (
+              <NavListItem>
+                <MenuLink>
+                  <Link
+                    activeClass="active"
+                    to={url}
+                    spy={true}
+                    smooth={true}
+                    duration={700}
+                  >
+                    {name}
+                  </Link>
+                </MenuLink>
+              </NavListItem>
+            ))}
+          </NavList>
         </StyledLink>
         <StyledResumeButton
           href="/resume.pdf"
