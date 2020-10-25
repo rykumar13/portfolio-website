64e4adde7d018df5a045bd25928a9468d7407abc
Rajiv Kumar <rajivk@gentrack.com>
2020-10-25 13:57:27 +0000

    adding styling to section headers

--- src/styles/Heading.js
+++ src/styles/Heading.js
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import theme from './theme';
+const { colors } = theme;
 
 const Heading = styled.h3`
   position: relative;
@@ -7,6 +9,17 @@ const Heading = styled.h3`
   margin: 10px 0 30px;
   width: 100%;
   white-space: nowrap;
+  color: ${colors.navy};
+  &:after {
+    content: '';
+    display: block;
+    height: 1px;
+    width: 300px;
+    background-color: ${colors.navy};
+    position: relative;
+    top: 4px;
+    margin-left: 20px;
+  }
 `
 
 export default Heading
